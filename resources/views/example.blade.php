6af2db684597a552e000979991c9da022f4670a5
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2022-03-30 16:12:56 +0700

    add: master layout
    Signed-off-by: Ahmad Azwar Annas <ahmadazw2@gmail.com>

---
+++ resources/views/example.blade.php
@@ -0,0 +1,23 @@
+@extends('template/master')
+
+@section('title', 'Example')
+
+@section('css')
+    {{-- Import CSS Disini --}}
+@endsection
+
+@section('header')
+    <h1>Example</h1>
+@endsection
+
+@section('content')
+    <p>This is the content of the example page.</p>
+@endsection
+
+@section('footer')
+    <p>This is the footer.</p>
+@endsection
+
+@section('scripts')
+    {{-- Import JS Disini --}}
+@endsection
