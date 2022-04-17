554ba03ecf8cf38ad8c5ae8278ef837b074a9668
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2022-04-17 22:08:52 +0700

    add: admin dashboard
    Signed-off-by: Ahmad Azwar Annas <ahmadazw2@gmail.com>

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\TempatWisata;
+use App\Models\Kategori;
+use App\Models\Comment;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('admin.index');
+        // Jumlah tempat wisata
+        $tempatWisataCount = TempatWisata::count();
+
+        // Jumlah User
+        $usersCount = User::count();
+
+        // rata rata comment rating
+        $commentCount = Comment::count();
+        $ratingCount = Comment::sum('rating');
+        $rataRating = $ratingCount / $commentCount;
+
+
+        // komentar terbaru
+        $comments = Comment::with('user')->orderBy('created_at', 'desc')->take(1)->get();
+
+
+        return view('admin.index',  compact('tempatWisataCount', 'usersCount', 'rataRating', 'comments'));
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,7 +3,50 @@
 @section('title', 'Home')
 
 @section('content')
-    <p>Content</p>
+    <div class="container">
+        <div class="row my-3">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Total Users</h5>
+                        <h1 class="card-text text-center">{{ $usersCount }}</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Total Tempat Wisata</h5>
+                        <h1 class="card-text text-center">{{ $tempatWisataCount }}</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Rata-rata Rating</h5>
+                        <h1 class="card-text text-center">{{ $rataRating }}</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row my-3 d-flex justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Komentar Terbaru</h5>
+                        @foreach ($comments as $comment)
+                            <p class="card-text">{{ $comment->user->name }}</p>
+                            <p class="card-text text-muted">Rating : {{ $comment->rating }}</p>
+                            <p class="card-text">{{ $comment->comment }}</p>
+                            <hr>
+                            <p class="card-text text-muted">{{ $comment->created_at }}</p>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('scripts')
