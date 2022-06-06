27d8f85911c018153ff1b7b31473f32f27ba91af
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2022-06-07 00:35:34 +0700

    update: build discover dari list & add information

--- Flutter/kemakassar/lib/screens/home_screen.dart
+++ Flutter/kemakassar/lib/screens/home_screen.dart
@@ -7,206 +7,211 @@ class Discover {
   const Discover(this.image, this.title);
 }
 
+class Information {
+  final String title;
+  final String description;
+
+  const Information(this.title, this.description);
+}
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
+    // TODO: fetch item dari API
+    final List<Discover> discoverItems = [
+      const Discover('lib/assets/images/trans-studio.webp', 'Trans Studio Makassar'),
+      const Discover('lib/assets/images/mal-phinisi.jpg', 'Mal Phinisi'),
+      const Discover('lib/assets/images/pantai-losari.jpg', 'Pantai Losari'),
+    ];
+
+    final List<Information> informationItems = [
+      const Information('Information 1', 'Lorem ipsum dolor sit amet'),
+      const Information('Information 2', 'Lorem ipsum dolor sit amet'),
+      const Information('Information 3', 'Lorem ipsum dolor sit amet'),
+    ];
+
     return Scaffold(
       body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: const [
-              Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Text(
-                  'Discover',
-                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Icon(
-                  Icons.search,
-                  size: 32,
-                ),
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Expanded(
-                child: SizedBox(
-                    height: 213,
-                    // TODO: ubah ke Listview.Separated dan fetch item dari API
-                    child: ListView(
-                      padding: const EdgeInsets.all(10.0),
-                      scrollDirection: Axis.horizontal,
-                      children: <Widget>[
-                        Stack(
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(8.0),
-                              child: Image.asset(
-                                'lib/assets/images/trans-studio.webp',
-                              ),
-                            ),
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Container(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: const Text(
-                                      'Trans Studio Makassar',
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold),
-                                    ))
-                              ],
-                            )
-                          ],
-                        ),
-                        Stack(
-                          alignment: Alignment.bottomLeft,
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(8.0),
-                              child: Image.asset(
-                                'lib/assets/images/mal-phinisi.jpg',
-                              ),
-                            ),
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Container(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: const Text(
-                                      'Mal Phinisi Point',
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold),
-                                    ))
-                              ],
-                            )
-                          ],
-                        ),
-                        Stack(
-                          alignment: Alignment.bottomLeft,
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(8.0),
-                              child: Image.asset(
-                                'lib/assets/images/pantai-losari.jpg',
-                              ),
-                            ),
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Container(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: const Text(
-                                      'Pantai Losari',
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold),
-                                    ))
-                              ],
-                            )
-                          ],
-                        ),
-                      ],
-                    )),
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: const [
-              Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Text('Find New Places',
-                    style:
-                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-              ),
-              Padding(padding: EdgeInsets.all(10.0), child: Text('Show All'))
-            ],
-          ),
-          Container(
-              padding: const EdgeInsets.all(10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: const [
-                      Icon(
-                        Icons.hotel_outlined,
-                        size: 48,
-                      ),
-                      Text('Hotel')
-                    ],
-                  ),
-                  Column(
-                    children: const [
-                      Icon(
-                        Icons.nature_outlined,
-                        size: 48,
-                      ),
-                      Text('Nature')
-                    ],
-                  ),
-                  Column(
-                    children: const [
-                      Icon(
-                        Icons.beach_access_outlined,
-                        size: 48,
-                      ),
-                      Text('Beach')
-                    ],
-                  ),
-                  Column(
-                    children: const [
-                      Icon(
-                        Icons.museum_outlined,
-                        size: 48,
-                      ),
-                      Text('Museum')
-                    ],
-                  ),
-                  Column(
-                    children: const [
-                      Icon(
-                        Icons.shopping_bag_outlined,
-                        size: 48,
-                      ),
-                      Text('Shopping')
-                    ],
-                  ),
-                ],
-              )),
-          Row(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: ListView(
+
             children: [
               Column(
-                children: const [
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Text('Information',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold)),
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                          'Discover',
+                          style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Icon(
+                          Icons.search,
+                          size: 32,
+                        ),
+                      )
+                    ],
+                  ),
+                  Container(
+                      height: 215,
+                      width: double.infinity,
+                      child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Container(
+                              width: 300,
+                              child: ListView.separated(
+                                scrollDirection: Axis.horizontal,
+                                itemCount: discoverItems.length,
+                                itemBuilder: (context, index) {
+                                  final item = discoverItems[index];
+
+                                  return Container(
+                                      width: 300,
+                                      child: Stack(
+                                        alignment: Alignment.bottomLeft,
+                                        children: [
+                                          ClipRRect(
+                                            borderRadius: BorderRadius.circular(8.0),
+                                            child: Image.asset(
+                                                item.image,
+                                                height: 215,
+                                                width: 300,
+                                                fit: BoxFit.fill
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(10.0),
+                                            child:
+                                            Text(
+                                                item.title,
+                                                style: const TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 20,
+                                                    color: Colors.white
+                                                )
+                                            ),
+                                          )
+                                        ],
+                                      )
+                                  );
+                                },
+                                separatorBuilder: (context, index) => const Padding(padding: EdgeInsets.all(5.0)),
+                              )
+                          )
+                      )
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Text('Find New Places',
+                            style:
+                            TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                      ),
+                      Padding(padding: EdgeInsets.all(10.0), child: Text('Show All'))
+                    ],
+                  ),
+                  Container(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            children: const [
+                              Icon(
+                                Icons.hotel_outlined,
+                                size: 48,
+                              ),
+                              Text('Hotel')
+                            ],
+                          ),
+                          Column(
+                            children: const [
+                              Icon(
+                                Icons.nature_outlined,
+                                size: 48,
+                              ),
+                              Text('Nature')
+                            ],
+                          ),
+                          Column(
+                            children: const [
+                              Icon(
+                                Icons.beach_access_outlined,
+                                size: 48,
+                              ),
+                              Text('Beach')
+                            ],
+                          ),
+                          Column(
+                            children: const [
+                              Icon(
+                                Icons.museum_outlined,
+                                size: 48,
+                              ),
+                              Text('Museum')
+                            ],
+                          ),
+                          Column(
+                            children: const [
+                              Icon(
+                                Icons.shopping_bag_outlined,
+                                size: 48,
+                              ),
+                              Text('Shopping')
+                            ],
+                          ),
+                        ],
+                      )),
+                  Row(
+                    children: [
+                      Column(
+                        children: const [
+                          Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Text('Information',
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold)),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                  // TODO: fix scrolling mengikuti height listview root bukan dari container
+                  Container(
+                    height: 300,
+                    padding: const EdgeInsets.all(10.0),
+                    child: ListView.separated(
+                      itemCount: informationItems.length,
+                      itemBuilder: (context, index) {
+                        final item = informationItems[index];
+
+                        return ListTile(
+                          title: Text(item.title),
+                          subtitle: Text(item.description),
+                        );
+                      },
+                      separatorBuilder: (context, index) => const Padding(padding: EdgeInsets.all(5.0)),
+                    ),
                   )
                 ],
-              ),
-              //  TODO: buat listview builder dengan item berdasarkan dari API
+              )
             ],
-          )
-        ],
-      )),
+          ),
+        )
+      ),
     );
   }
 }
